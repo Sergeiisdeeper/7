131dbf9a45d4a1980ead021daf8db8511acbcf6a
Sergeiisdeeper <sergeimn777@gmail.com>
2024-12-12 05:00:09 +0000

    Serve static files in DEBUG mode

--- project7/project7/urls.py
+++ project7/project7/urls.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin panel
-    path('', include('accounts.urls')),  # Include URLs from the accounts app
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+]
+
+# Serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
